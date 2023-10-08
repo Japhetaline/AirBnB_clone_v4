1761150c17a0c193ecfde2b9e2eff24de9939aae
primeace7 <osaruonamenjeffery@gmail.com>
2023-10-08 22:14:28 +0000

    Add task 4 file

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,10 @@
-//
+$(document).ready(() => {
+  $('div.popover').on('click', () => {
+    const checked = $('li.amenity:checked');
+    const display = [];
+    checked.each(function (index, val) {
+      display.push(val);
+    });
+    $('div.amenities h4').text(display);
+  });
+});
